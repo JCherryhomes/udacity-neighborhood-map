d6e83c3edcfeb4cff8c58cfd199cfecc221f8161
Jonathan Cherryhomes <cherryhomesj@gmail.com>
2019-01-07 21:20:50 -0600

    Updated the application to pass the map points as an array that can be filtered

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
-import { Map } from './components/Map/Map';
-import './App.css';
-import { 
-  CircularProgress, 
-  AppBar, 
+import React, { Component } from "react";
+import { Map } from "./components/Map/Map";
+import "./App.css";
+import {
+  CircularProgress,
+  AppBar,
   Toolbar,
   IconButton,
   Typography,
   Paper
-} from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
-import { SideNavigation } from './components/SideNavigation/SideNavigation';
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import { SideNavigation } from "./components/SideNavigation/SideNavigation";
 
 class App extends Component {
-
   constructor(props) {
     super(props);
 
@@ -27,13 +26,13 @@ class App extends Component {
 
   toggleDrawer(event, isOpen) {
     this.setState({
-        left: isOpen
+      left: isOpen
     });
   }
 
-  selectMapPoint(mapPoint) {
+  filterMapPoints(mapPoints) {
     this.setState({
-      mapPoint: mapPoint,
+      mapPoints: mapPoints,
       left: false
     });
   }
@@ -43,16 +42,19 @@ class App extends Component {
       return (
         <div>
           <h2>Loading Maps API</h2>
-          <CircularProgress></CircularProgress>
+          <CircularProgress />
         </div>
-      )
+      );
     }
 
     return (
       <div className="App">
         <AppBar position="absolute">
           <Toolbar>
-            <IconButton color='inherit' onClick={(event) => this.toggleDrawer(event, true)}>
+            <IconButton
+              color="inherit"
+              onClick={event => this.toggleDrawer(event, true)}
+            >
               <MenuIcon />
             </IconButton>
             <Typography variant="title" color="inherit" noWrap>
@@ -60,15 +62,19 @@ class App extends Component {
             </Typography>
           </Toolbar>
         </AppBar>
-        <SideNavigation open={this.state.left} onClose={this.toggleDrawer} onSelect={(data) => this.selectMapPoint(data)}></SideNavigation>
+        <SideNavigation
+          open={this.state.left}
+          onClose={this.toggleDrawer}
+          onFilter={data => this.filterMapPoints(data)}
+        />
         <main>
-          <Map google={this.props.google} mapPoint={this.state.mapPoint}></Map>
-        </main>  
+          <Map google={this.props.google} mapPoints={this.state.mapPoints} />
+        </main>
         <Paper className="ta_logo" elevation={1}>
           <Typography variant="subheading" color="inherit" noWrap>
             Made with <a href="http://here.com">HERE.com</a>
           </Typography>
-        </Paper>    
+        </Paper>
       </div>
     );
   }
